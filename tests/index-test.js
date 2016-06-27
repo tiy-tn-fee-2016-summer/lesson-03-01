04395dae38911b82cc1d5274eddbf6b52f21a591
Ryan Tablada <ryan.tablada@gmail.com>
2016-06-27 11:33:20 -0500

    Test add

--- tests/index-test.js
+++ tests/index-test.js
@@ -1,3 +1,15 @@
 'use strict';
 
 /* global require */
+
+QUnit.module('Addition');
+
+const add = require('add');
+
+test('we can add numbers', (assert) => {
+  const result = add(1, 3);
+  const result2 = add(1, 5);
+
+  assert.equal(result, 4);
+  assert.equal(result2, 6);
+});
